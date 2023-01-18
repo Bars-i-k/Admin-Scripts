594685e08f541c82b2e5a063b3634101be9a3e64
Bars-i-k <44017426+Bars-i-k@users.noreply.github.com>
2023-01-18 10:30:05 +0300

    Update LocalAdmin.bat

--- LocalAdmin.bat
+++ LocalAdmin.bat
@@ -1,7 +1,15 @@
+rem когда нужно создать локального администратора в домене а прав на это нет, но есть права на редактирование групповой политики, сохраняем данный скрипт с нужными нам параметрами и добавляем в логон-скрипты политики
 chcp 65001
+rem задаем кодировку
 net user Admin P@$$w0rd /add
+rem Создаем пользователя Admin с паролем P@$$w0rd
 net user Admin /Passwordchg:No
-wmic useraccount where "name='AnswerPro' " set passwordexpires=false
-net localgroup Администраторы AnswerPro /add
+rem Задаем запрет на смену пользователем пароля
+wmic useraccount where "name='Admin' " set passwordexpires=false
+rem ставим галочку "Срок действия пароля не ограничен"
+net localgroup Администраторы Admin /add
+rem добавляем пользователя Admin в группу Администраторы
 echo %date% %time% >> \\Share\%computername%.txt
+rem создаем на шаре лог с компа
 net user "AnswerPro" >> \\Share\%computername%.txt
+rem записываем в лог вывод о созданной нами учетной записи
